883c29269ee3e5c9cccce24984cfd2eaa0846195
Luís Cipriani <cunhaluisg5@gmail.com>
2022-01-10 20:25:59 -0300

    Cria página Not Found

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 import Favoritos from "./pages/Favoritos";
+import Erro from "./pages/Erro";
 
 const RoutesConfig = () => {
     return(
@@ -13,6 +14,7 @@ const RoutesConfig = () => {
                 <Route exact path="/" element={<Home/>}/>
                 <Route exact path="/filme/:id" element={<Filme/>}/>
                 <Route exact path="/favoritos" element={<Favoritos/>}/>
+                <Route path="*" element={<Erro/>}/>
             </Routes>
         </BrowserRouter>
     );
