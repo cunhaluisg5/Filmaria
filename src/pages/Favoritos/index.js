a967ddfc8681454385e7bd0993f7fa5c7df1e8f0
Luís Cipriani <cunhaluisg5@gmail.com>
2022-01-10 19:52:27 -0300

    Cria página favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+
+import "./favoritos.css";
 
 function Favoritos(){
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        const minhaLista = localStorage.getItem("filmes");
+        setFilmes(JSON.parse(minhaLista) || []);
+
+    }, []);
+
+    function handleDelete(id){
+        let filtroFilmes = filmes.filter((item) => {
+            return (item.id !== id);
+        });
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem("filmes", JSON.stringify(filtroFilmes));
+    }
+
+
     return(
-        <div>
-            <h1>Página Favoritos</h1>
+        <div id="meus-filmes">
+            <h1>Meus Filmes</h1>
+
+            <ul>
+                {filmes.map((item) => {
+                    return(
+                        <li key={item.id}>
+                            <span>{item.nome}</span>
+
+                            <div>
+                                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                                <button onClick={() => {handleDelete(item.id)}}>Excluir</button>
+                            </div>
+
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 };
